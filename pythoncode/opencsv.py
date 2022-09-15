437b926e672734fa54cc3e84b6f5ce4f86f521f8
Ellie Pizey <ellie.pizey@gmail.com>
2022-09-15 15:36:39 +0100

    Onwards

--- pythoncode/opencsv.py
+++ pythoncode/opencsv.py
@@ -1,13 +1,3 @@
 import csv
 
-with open('EUROVENT_AC.scv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=';')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+#creating dictionary to read as csv:
