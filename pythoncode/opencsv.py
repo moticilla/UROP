7add5d483d9beebd750bc37cb47184cdb3d316a9
Ellie Pizey <ellie.pizey@gmail.com>
2022-09-16 11:36:06 +0100

    Onwards

--- pythoncode/opencsv.py
+++ pythoncode/opencsv.py
@@ -1,3 +1,15 @@
 import csv
 
-#creating dictionary to read as csv:
+#with open('my_csv_experiment.csv', mode='r') as infile:
+#    reader = csv.reader(infile)
+#    with open("/Users/Documents/UROP/my_csv_experiment.csv", mode='w') as outfile:
+#        writer = csv.writer(outfile)
+#        mydict = {rows[0]:rows[5] for rows in reader}
+
+#experiment_dict = csv.DictReader(open("my_csv_experiment.csv"))
+
+with open("/home/elliep/Documents/UROP/my_csv_experiment.csv", mode='r') as infile:
+    reader = csv.reader(infile)
+    with open("/home/elliep/Documents/UROP/my_csv_experiment.csv", mode='w') as outfile:
+        writer = csv.writer(outfile)
+        mydict = {rows[0]:rows[5] for rows in reader}
