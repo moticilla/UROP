d1c258ac42df259e368ef955a097ab4489512a4a
Ellie Pizey <ellie.pizey@gmail.com>
2022-09-16 11:37:59 +0100

    ONwards

--- pythoncode/opencsv.py
+++ pythoncode/opencsv.py
@@ -8,8 +8,8 @@ import csv
 
 #experiment_dict = csv.DictReader(open("my_csv_experiment.csv"))
 
-with open("/home/elliep/Documents/UROP/my_csv_experiment.csv", mode='r') as infile:
+with open("/elliep/Documents/UROP/my_csv_experiment.csv", mode='r') as infile:
     reader = csv.reader(infile)
-    with open("/home/elliep/Documents/UROP/my_csv_experiment.csv", mode='w') as outfile:
+    with open("/elliep/Documents/UROP/my_csv_experiment.csv", mode='w') as outfile:
         writer = csv.writer(outfile)
         mydict = {rows[0]:rows[5] for rows in reader}
