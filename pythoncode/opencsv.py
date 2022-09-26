0b91475031d9c05dd514d55c9c29073d76e13f51
Ellie Pizey <ellie.pizey@gmail.com>
2022-09-26 23:56:16 +0100

    first graphs made

--- pythoncode/opencsv.py
+++ pythoncode/opencsv.py
@@ -31,16 +31,32 @@ EUROVENTdf = pd.read_csv('EUROVENT_AC_tr.csv')
 
 df2 = pd.DataFrame(df.iloc[:,1])
 df2.insert(0, "CondD", [21 for n in range(df2.shape[0])], True)
-print(df2)
+#print(df2)
 
 CondB = pd.DataFrame(EUROVENTdf.iloc[:,47])
-CondB.insert(0, "CondB", [30 for n in range(CondB.shape[0])], True)
-print(CondB)
+CondB.insert(0, "temp", [30 for n in range(CondB.shape[0])], True)
+CondB = CondB.rename(columns={"temp":"Temp",'Cooling PL CondB | EER @ 30°C ':"EER"})
+#print(CondB)
 
 CondC = pd.DataFrame(EUROVENTdf.iloc[:,51])
-CondC.insert(0, "CondB", [25 for n in range(CondC.shape[0])], True)
-print(CondC)
+CondC.insert(0, "temp", [25 for n in range(CondC.shape[0])], True)
+CondC = CondC.rename(columns={"temp":"Temp",'Cooling PL CondC | EER @ 25°C':"EER"})
+#print(CondC)
 
 CondD = pd.DataFrame(EUROVENTdf.iloc[:,55])
-CondD.insert(0, "CondB", [20 for n in range(CondD.shape[0])], True)
-print(CondD)
+CondD.insert(0, "temp", [20 for n in range(CondD.shape[0])], True)
+CondD = CondD.rename(columns={"temp":"Temp",'Cooling PL CondD | EER @ 20°C ':"EER"})
+#print(CondD)
+
+frames = [CondB, CondC, CondD]
+
+result = pd.concat(frames)
+#result.plot(kind='scatter',x='Temp',y='EER',color='red')
+#plt.show()
+
+b = CondB["EER"].mean()
+c = CondC["EER"].mean()
+d = CondD["EER"].mean()
+
+plt.plot([30,25,20],[b, c, d],"o")
+plt.show()
