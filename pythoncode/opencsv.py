4d71e2cfc3fb8babeb7cf501f9d3a68e57cc9302
Ellie Pizey <ellie.pizey@gmail.com>
2022-09-29 10:54:04 +0100

    Onwards

--- pythoncode/opencsv.py
+++ pythoncode/opencsv.py
@@ -1,3 +1,4 @@
+from cmath import nan
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -37,16 +38,19 @@ df2.insert(0, "CondD", [21 for n in range(df2.shape[0])], True)
 CondB = pd.DataFrame(EUROVENTdf.iloc[:,47])
 CondB.insert(0, "temp", [30 for n in range(CondB.shape[0])], True)
 CondB = CondB.rename(columns={"temp":"Temp",'Cooling PL CondB | EER @ 30°C ':"EER"})
+CondB = CondB.loc[np.isnan(CondB["EER"]) == False]
 #print(CondB)
 
 CondC = pd.DataFrame(EUROVENTdf.iloc[:,51])
 CondC.insert(0, "temp", [25 for n in range(CondC.shape[0])], True)
 CondC = CondC.rename(columns={"temp":"Temp",'Cooling PL CondC | EER @ 25°C':"EER"})
+CondC = CondC.loc[np.isnan(CondC["EER"]) == False]
 #print(CondC)
 
 CondD = pd.DataFrame(EUROVENTdf.iloc[:,55])
 CondD.insert(0, "temp", [20 for n in range(CondD.shape[0])], True)
 CondD = CondD.rename(columns={"temp":"Temp",'Cooling PL CondD | EER @ 20°C ':"EER"})
+CondD = CondD.loc[np.isnan(CondD["EER"]) == False]
 #print(CondD)
 
 frames = [CondB, CondC, CondD]
@@ -71,3 +75,6 @@ derr =np.std(CondD.iloc[:,1])
 
 plt.errorbar([30,25,20],[b, c, d],linestyle = ":", yerr = [berr, cerr, derr])
 plt.show()
+
+
+#data = data.loc[(data["deaths_7_days"] > 0) & (data["deaths_24_hours"] > 0)]
